5d044fbd5a21a97882acebbf27994a2fa4fc1b5a
devhoss <velasco2288@gmail.com>
2025-08-21 00:48:58 -0500

    unit test mockito getall

--- src/test/java/com/devhoss/ProductServiceMockTest.java
+++ src/test/java/com/devhoss/ProductServiceMockTest.java
@@ -11,9 +11,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
 
 @QuarkusTest
 public class ProductServiceMockTest {
@@ -37,10 +40,29 @@ public class ProductServiceMockTest {
                 .statusCode(201)
                 .body("name", equalTo("Laptop"))
                 .body("price", equalTo(1200.0f));
+    }
+
+    @Test
+    void testGetAllProducts() {
+        List<Product> mockProducts = List.of(
+                new Product(1L, "Laptop", 1299.99),
+                new Product(2L, "Phone", 799.99)
+        );
+
+        Mockito.when(mockProductService.listAll()).thenReturn(mockProducts);
+
+        given()
+                .when().get("/products")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(2))
+                .body("[0].name", equalTo("Laptop"))
+                .body("[1].price", equalTo(799.99f));
 
 
 
     }
+
 
 
 }
